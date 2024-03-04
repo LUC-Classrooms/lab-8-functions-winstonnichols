512415ab13cc76847f65016f6b3c6e99fc6d3ade
Winston Nichols <157043193+winstonnichols@users.noreply.github.com>
2024-03-04 01:07:39 +0000

    Lab 8 complete

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ function setup() {
 }
 
 function draw() {
-  background(200);
+  background(255, 192, 203);
 
   //this is a call to myShape()
-  myShape(width / 2, height / 2, 1);
+  myShape(300, 300, 50);
+  myShape(300, 200, 60);
+  myShape(300, 50, 70);
+  
   // myShape() takes three arguments
   // arg1: horizontal location
   // arg2: vertical location
@@ -16,13 +19,18 @@ function draw() {
   
 }
 
-function myShape(x, y, s) {
+function myShape(x, y, w,) {
   // make this function more interesting
   push(); // make a separate layer
   translate(x, y); // move the origin point
-  scale(s);
-  
-  ellipse(0, 0, w, h); // simple ellipse at the translated origin (0,0)
+  scale(w);
+  rectMode(CENTER);
+  rect(0, 0, .5);
+  fill(100, 100, 200);
+  ellipse(0, 0, .5); // simple ellipse at the translated origin (0,0)
+  fill(0,0,255);
+  ellipse(-3, -2, .5);
+  ellipse(3, -2, .5);
   
   pop(); // dispose of the layer
 }
